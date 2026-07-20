dbe7af41de625e2547f34a76038a68c08fbb98ff
Hu-Tao128 <alcantarahuerta128@gmail.com>
2026-07-19 23:56:43 -0700

    feat: Agregar cors de Vercel

--- config/settings.py
+++ config/settings.py
@@ -156,5 +156,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://comunidad-conectada-self.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
